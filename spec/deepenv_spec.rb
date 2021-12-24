94589e8df05641b55794af22483210b6cb1af663
Rene <renwooller@gmail.com>
2021-12-24 18:23:50 +1000

    initial commit

---
+++ spec/deepenv_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Deepenv do
+  it "has a version number" do
+    expect(Deepenv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
